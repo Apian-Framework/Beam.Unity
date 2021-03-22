05308149592b4ab3d3f0ea1aeccf791b55bf3371
jim berry <jim@bigfattail.com>
2021-03-22 15:21:49 -0400

    GroupType dropdown for new game

--- Assets/Scripts/UI/SelGamePanel.cs
+++ Assets/Scripts/UI/SelGamePanel.cs
@@ -8,8 +8,10 @@ using Apian;
 
 public class SelGamePanel : MovableUICanvasItem
 {
-    public GameObject newGameField;
     public GameObject existingGameDrop;
+
+    public GameObject newGameField;
+    public GameObject agreeTypeDrop;
     public const string kNoGames = "No Games Found";
 
     protected IDictionary<string, BeamGameInfo> existingGames;
@@ -21,6 +23,10 @@ public class SelGamePanel : MovableUICanvasItem
         existingGames = existingGameDict;
         frontEnd = BeamMain.GetInstance().frontend;
 
+        TMP_Dropdown typeDrop = agreeTypeDrop.GetComponent<TMP_Dropdown>();
+        typeDrop.ClearOptions();
+        typeDrop.AddOptions( new List<string>(){LeaderSezGroupManager.kGroupType});
+
         // newGameField goes to default prompt. TODO: should there be a default/suggestion?
         // newGameField.GetComponent<TMP_InputField>().text = ???;
         TMP_Dropdown existingDrop = existingGameDrop.GetComponent<TMP_Dropdown>();
@@ -29,6 +35,11 @@ public class SelGamePanel : MovableUICanvasItem
         if (existingGames?.Count > 0)
         {
             existingDrop.AddOptions(existingGames.Keys.ToList<string>());
+        }
+        else
+        {
+            existingDrop.AddOptions( new List<string>(){"(No existing games to join.)"});
+            existingDrop.enabled = false;
         }
 
         moveOnScreen();
@@ -48,8 +59,9 @@ public class SelGamePanel : MovableUICanvasItem
     {
         moveOffScreen();
         string newGameName = newGameField.GetComponent<TMP_InputField>().text;
-        // TODO: THIS is AWFUL!!
-        BeamGameInfo newGameInfo = frontEnd.beamAppl.beamGameNet.CreateBeamGameInfo(newGameName, LeaderSezGroupManager.groupType);
+        string agreementType = agreeTypeDrop.GetComponent<TMP_Dropdown>().captionText.text;
+
+        BeamGameInfo newGameInfo = frontEnd.beamAppl.beamGameNet.CreateBeamGameInfo(newGameName, agreementType);
 
         frontEnd.OnGameSelected(newGameInfo, GameSelectedArgs.ReturnCode.kCreate );
     }
