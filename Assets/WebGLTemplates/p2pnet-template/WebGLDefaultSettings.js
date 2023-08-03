8173b97c0c49d688ac0c831cb986c21b3fb2fce1
jim berry <jim@bigfattail.com>
2023-08-03 17:19:18 -0400

    Added a couple of accounts to the defaults so they'll always be there for testing

--- Assets/WebGLTemplates/p2pnet-template/WebGLDefaultSettings.js
+++ Assets/WebGLTemplates/p2pnet-template/WebGLDefaultSettings.js
@@ -13,7 +13,10 @@ var WebGLDefaultSettings = {
     "Gnosis Main": "{\"RpcUrl\": \"https://rpc.gnosischain.com\", \"ChainId\": 100, \"Currency\": \"xDAI\"}",
     "Gnosis Chiado": "{\"RpcUrl\": \"https://rpc.chiadochain.net\", \"ChainId\": 10200, \"Currency\": \"xDAI\"}"
   },
-  "gameAcctJSON": { },
+  "gameAcctJSON": {
+    "0xe3BC1CDEc4274f880D870bD32C86514f1971b841": "{\"Type\":1,\"Address\":\"0xe3BC1CDEc4274f880D870bD32C86514f1971b841\",\"Data\":\"{\\\"crypto\\\":{\\\"cipher\\\":\\\"aes-128-ctr\\\",\\\"ciphertext\\\":\\\"9efb352ec8c03e0a93042e0aabb421af189e9632ab8bfb2859c77d69d448f29e\\\",\\\"cipherparams\\\":{\\\"iv\\\":\\\"b538fcfe4a2d778a2ac2bf1422e82697\\\"},\\\"kdf\\\":\\\"scrypt\\\",\\\"mac\\\":\\\"c47ed97ce9055d64d33925f11eb29af0a5541f35d1c2927eb369abc15bd0c710\\\",\\\"kdfparams\\\":{\\\"n\\\":262144,\\\"r\\\":1,\\\"p\\\":8,\\\"dklen\\\":32,\\\"salt\\\":\\\"c3270c68f862d0057228da1c999534987b9596a5292e6cdd68ab04b90ec7060e\\\"}},\\\"id\\\":\\\"b3624fc5-4e45-44f2-b17b-fdbe1395181f\\\",\\\"address\\\":\\\"0xe3BC1CDEc4274f880D870bD32C86514f1971b841\\\",\\\"version\\\":3}\"}",      
+    "0x4e921F3d5170e5d45755Fd5801ee57F64457BEA9": "{\"Type\":1,\"Address\":\"0x4e921F3d5170e5d45755Fd5801ee57F64457BEA9\",\"Data\":\"{\\\"crypto\\\":{\\\"cipher\\\":\\\"aes-128-ctr\\\",\\\"ciphertext\\\":\\\"a4bad7fd5c0869fa1f9bc3234a6ac9a94a97f6df1d2c73b24a6240d01fcaf680\\\",\\\"cipherparams\\\":{\\\"iv\\\":\\\"a69e2216f40d9bf1ef16b00f459d0bbe\\\"},\\\"kdf\\\":\\\"scrypt\\\",\\\"mac\\\":\\\"110ebeb71e85b58da48188ec659938846e4e4494aefb76a14f6edb4a5e6c4d9a\\\",\\\"kdfparams\\\":{\\\"n\\\":262144,\\\"r\\\":1,\\\"p\\\":8,\\\"dklen\\\":32,\\\"salt\\\":\\\"14cd4b5863892e8ea97cced2e69725392c398077a9dcd182d6932b275557945e\\\"}},\\\"id\\\":\\\"21e2119b-360c-4a25-9f32-9a6acc61f260\\\",\\\"address\\\":\\\"0x4e921F3d5170e5d45755Fd5801ee57F64457BEA9\\\",\\\"version\\\":3}\"}"
+  },
   "curP2pConnection": "PokeyHedgehog MQTT WS",
   "curBlockchain": "Gnosis Chiado",
   "anchorContractAddr": "0xCD23D5C57E88c829d6413a0Da55f3c627900BF64",  
